f4b0fd8a9e7004e53216b20e7096b33539fcd43c
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-11-08 23:11:58 +0530

    setup eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import React from 'react'
 import { Poppins } from "next/font/google"
 
 const poppins = Poppins({
